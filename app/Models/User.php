625c431bb830de7a085b10ddeb8fccd649a0fa27
DDorka02 <dobszaydorka@gmail.com>
2025-01-09 13:21:03 +0100

    javítás a User-nél

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
