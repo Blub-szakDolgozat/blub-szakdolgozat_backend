f016ffa782184f30baccc71c29477478d6ac3221
horvathnauzika <naihorvath@gmail.com>
2025-01-14 12:19:19 +0100

    feliratkozás tábla

---
+++ database/migrations/2025_01_14_111233_create_feliratkozas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feliratkozas', function (Blueprint $table) {
+            $table->unsignedBigInteger('felhasznalo'); 
+            $table->unsignedBigInteger('esemeny'); 
+            $table->timestamp('feliratkozás_dátuma')->nullable();
+
+            $table->primary(['felhasznalo', 'esemeny']);
+
+            // Külső kulcsok definiálása
+            $table->foreign('felhasznalo')->references('azonosito')->on('users')->onDelete('cascade');
+            $table->foreign('esemeny')->references('esemény_id')->on('esemenies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feliratkozas');
+    }
+};
