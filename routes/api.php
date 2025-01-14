26b979cf10d3a0eefdb0a3c21133a80b5d6fface
horvathnauzika <naihorvath@gmail.com>
2025-01-14 14:11:49 +0100

    alap lekérdezések api végpontok

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\CikkController;
+use App\Http\Controllers\EsemenyController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VideoController;
 use App\Http\Controllers\VizilenyekController;
 use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
@@ -11,6 +14,56 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Alap lekérdezések --> Cikk
+// Cikkeklekérdezése:
+Route::get('/cikkek', [CikkController::class, 'index']);
+
+//Új cikk hozzáadása:
+Route::post('/cikk-add', [CikkController::class, 'store']);
+
+//Cikk id alapján törlése:
+Route::delete('/cikk-torol/{cikk_id}', [CikkController::class, 'destroy']);
+
+//Cikk id alapján lekérdezése:
+Route::get('/cikk-show/{cikk_id}', [CikkController::class, 'show']);
+
+//Cikk id alapján részlegesen frissítése:
+Route::put('/cikkek/{cikk_id}', [CikkController::class, 'put']);
+
+
+// Alap lekérdezések --> Esemény
+// Események lekérdezése:
+Route::get('/esemenyek', [EsemenyController::class, 'index']);
+
+//Új esemény hozzáadása:
+Route::post('/esemeny-add', [EsemenyController::class, 'store']);
+
+//esemény id alapján törlése:
+Route::delete('/esemeny-torol/{esemény_id}', [EsemenyController::class, 'destroy']);
+
+//esemény id alapján lekérdezése:
+Route::get('/esemeny-show/{esemény_id}', [EsemenyController::class, 'show']);
+
+//esemény id alapján részlegesen frissítése:
+Route::put('/esemenyek/{esemény_id}', [EsemenyController::class, 'put']);
+
+
+// Alap lekérdezések --> Videók
+// Videók lekérdezése:
+Route::get('/videok', [VideoController::class, 'index']);
+
+//Új videó hozzáadása:
+Route::post('/video-add', [VideoController::class, 'store']);
+
+//videó id alapján törlése:
+Route::delete('/video-torol/{video_id}', [VideoController::class, 'destroy']);
+
+//videó id alapján lekérdezése:
+Route::get('/video-show/{video_id}', [VideoController::class, 'show']);
+
+//videó id alapján részlegesen frissítése:
+Route::put('/videok/{video_id}', [VideoController::class, 'put']);
 
 
 // Autentikált útvonal, simple user is:
