07367ddf4a545d8bfabbda1ac7fc0e1923afbae3
DDorka02 <dobszaydorka@gmail.com>
2025-01-31 14:26:34 +0100

    cikk hozzáadása

--- database/migrations/2025_01_07_132313_create_cikks_table.php
+++ database/migrations/2025_01_07_132313_create_cikks_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -19,6 +20,13 @@ return new class extends Migration
             $table->date('publikalva')->default('2025-01-07');
             $table->timestamps();
         });
+        DB::table('cikks')->insert([
+            'cim' => 'Az óceánok szemétproblémája és megoldások',
+            'kepek' => 'kepek/cikk1.jpg',
+            'leiras' => 'Az óceánok az élet forrásai és az egész bolygónk számára elengedhetetlenek. Azonban sajnos az óceánok napról napra egyre inkább veszélyeztetettek a szemét és a szennyezés miatt. Az óceánokba naponta millió tonna műanyag hulladék és szemét kerül, ami súlyos környezeti problémákat okoz. Ez a cikk arról fog szólni, hogy miért van szükségünk az óceánok védelmére, és hogyan segíthetünk az óceánok megtisztításában és megőrzésében.',
+            'publikalva' => '2025-06-08',
+        ]);
+   
     }
 
     /**
