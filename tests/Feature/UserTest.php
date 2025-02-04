fa93f49415dd5b013dfd5426eb51a7e4c17879f1
midnightcrow00 <orsolyaalbert1@gmail.com>
2025-02-04 14:14:19 +0100

    vizileny teszt

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -43,5 +43,28 @@ class UserTest extends TestCase
         $response = $this->get('/api/videok-hossza/');
         $response->assertStatus(200);
     }
+    public function test_get_vizilenyek() : void {
+        $response = $this->get('/api/vizilenyek/');
+       $response->assertStatus(200);
+   }
+public function test_store_vizilenyek() : void {
+$response = $this->post('/api/vizilenyek-add', [
+   'esemeny_neve' => 'Teszt Esemény',
+   'datum' => '2025-02-10',
+   'helyszin' => 'Teszt Helyszín',
+   'letszam' => 10
+]);
+$response->assertStatus(201); // Ha sikeres létrehozás, akkor 201-et várunk
+}
+public function test_update_vizilenyek() : void {
+$response = $this->put('/api/vizilenyek/1', [
+   'esemeny_neve' => 'Frissített Esemény'
+]);
+$response->assertStatus(200); // Ha sikeres frissítés, akkor 200-at várunk
+}
+public function test_delete_vizilenyek() : void {
+$response = $this->delete('/api/vizilenyek-torol/1');
+$response->assertStatus(200); // Ha sikeres törlés, akkor 200-at várunk
+}
 
 }
