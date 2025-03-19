fb71987d194d7f6f1ef499e9d646bcf6735db5bb
horvathnauzika <naihorvath@gmail.com>
2025-03-19 14:26:36 +0100

    több cikk hozzáadása

--- database/migrations/2025_01_07_132313_create_cikks_table.php
+++ database/migrations/2025_01_07_132313_create_cikks_table.php
@@ -35,6 +35,23 @@ return new class extends Migration
             Az éghajlatváltozás egyik kulcselemét a Föld vízkörforgására gyakorolt hatás jelenti; e körforgás végzi a víz folyamatos elosztását az óceánokból a légkörbe, szárazföldre, folyókba és tavakba, majd vissza a tengerekbe és az óceánokba.',
             'publikalva' => '2025-03-13',
         ]);
+        DB::table('cikks')->insert([
+            'cim' => 'Telítődnek az óceánok a műanyaggal',
+            'kepek' => 'kepek/cikk3.jpg',
+            'leiras' => 'Évente nyolcmillió tonna műanyag kerül a tengerekbe. A szennyezés visszafordíthatatlan károkat okozhat az óceánok élővilágában - figyelmeztet az ENSZ Környezetvédelmi Programja (UNEP).
+            Az óceánok kritikus helyzete az egyik fő téma az ENSZ Nairobiban zajló, háromnapos környezetvédelmi konferenciáján (UNEA) is. A tanácskozáson az ENSZ szakosított szervezete arra szólította fel az országok kormányait és a magánszektort, hogy mérsékeljék a műanyagból készült termékek gyártását. "A gyökerénél kell kezelni a problémát" - mondta Erik Solheim, az UNEP igazgatója.
+            Egyre több tengerparttal rendelkező ország ismeri fel a problémát és dönt az egyszer használható műanyag zacskók betiltása mellett, mint legutóbb Kenya is tette. Indonézia - ahol Kína után a világon a  legtöbb műanyagot gyártják - vállalta, hogy 2025-ig 75 százalékkal csökkenti az óceánba kerülő műanyag mennyiségét. Környezetvédelmi szervezetek szerint azonban mindez kevés: az ENSZ-nek menetrendet kell felállítania a tengerek műanyagszennyezésének teljes felszámolására. ',
+            'publikalva' => '2017-12-05',
+        ]);
+        DB::table('cikks')->insert([
+            'cim' => 'Egyre sürgetőbb az óceánok védelme!',
+            'kepek' => 'kepek/cikk4.jpg',
+            'leiras' => '2018 és 2022 között 8,5 százalékkal, csaknem 8,5 millió órára nőtt a halászati tevékenységgel töltött idő az óceánok nyílt vizein - derült ki a Greenpeace új jelentéséből, amely az óceánokat fenyegető veszélyekről készült.
+            „A tendenciák tehát azt mutatják, hogy a valóságban pont az ellenkezője történik, mint amit az óceánvédelmi egyezményben meghatároztak” - figyelmeztetnek a jelentésben, amely részletesen ismerteti azt is, hogy az óceánok felmelegedése, elsavasodása, szennyezése egyre nagyobb terhet ró a tenger élővilágára.
+            A jelentés szerint jelenleg a nyílt tengerek kevesebb mint 1 százalékát védik megfelelően, és ahhoz, hogy 2030-ra a világ óceánjainak 30 százaléka védett legyen, évente körülbelül 11 millió négyzetkilométernyi tengeri területet kell megóvni a pusztítástól.
+            Az ökológiai jelentőségük alapján a Greenpeace három nyílt tengeri területet javasol elsőként védetté nyilvánítani: a Csendes-óceán északnyugati részén található Emperor-fenékhegyvidéket, a Sargasso-tengert és az Ausztrália és Új-Zéland közötti Lord Howe-hátságot.',
+            'publikalva' => '2023-08-04',
+        ]);
    
     }
 
